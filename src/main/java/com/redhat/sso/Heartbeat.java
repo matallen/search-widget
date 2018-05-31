cf32227040c1fc19ef63c242761492ce2de93bd5
Mat Allen <mallen@redhat.com>
2018-05-31 12:33:16 -0500

    update to ping code

--- src/main/java/com/redhat/sso/Heartbeat.java
+++ src/main/java/com/redhat/sso/Heartbeat.java
@@ -1,8 +1,9 @@
 package com.redhat.sso;
 
-import static com.jayway.restassured.RestAssured.given;
-
-import java.util.Properties;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Properties;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -14,14 +15,11 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 import org.apache.log4j.Logger;
-import org.codehaus.groovy.control.messages.Message;
-
-import com.jayway.restassured.response.Response;
 
 public class Heartbeat {
   private static final Logger log = Logger.getLogger(Heartbeat.class);
   private static Timer t;
-
+  
   public static void runOnce(){
     new HeartbeatRunnable().run();
   }
@@ -43,31 +41,40 @@ public class Heartbeat {
       String url=System.getenv("PING_URL");
       
       if (url==null || "".equals(url)) return;
-      
-      Response response=given().get(url).andReturn();
-      log.debug("Ping: called '"+url+"', response code was: "+response.getStatusCode());
-      if (200!=response.getStatusCode()){
-        Properties props = new Properties();
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
-
-        Session session = Session.getInstance(props);
-
-        try {
-
-            MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("mojo-search-widget@redhat.com"));
-            message.setRecipients(MimeMessage.RecipientType.TO, InternetAddress.parse("mallen@redhat.com"));
-            message.setSubject("Health Check Ping Failure: code="+response.getStatusCode());
-            message.setText("Hit the url \""+url+"\" and got the response code \""+response.getStatusCode()+"\" and payload \""+response.getBody().asString()+"\"");
-            Transport.send(message);
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
+        try{
+        URL obj=new URL(url);
+        HttpURLConnection con=(HttpURLConnection)obj.openConnection();
+        con.setRequestMethod("GET");
+        int responseCode=con.getResponseCode();
+  
+        log.debug("Ping: called '"+url+"', response code was: "+responseCode);
+        if (200!=responseCode){
+          BufferedReader in=new BufferedReader(new InputStreamReader(con.getInputStream()));
+          String inputLine;
+          StringBuffer response=new StringBuffer();
+    
+          while ((inputLine=in.readLine()) != null){
+            response.append(inputLine);
+          }
+          in.close();
+          
+          Properties props = new Properties();
+          props.put("mail.smtp.starttls.enable", "true");
+          props.put("mail.smtp.auth", "true");
+          props.put("mail.smtp.host", "smtp.gmail.com");
+          props.put("mail.smtp.port", "587");
+          
+          Session session = Session.getInstance(props);
+          MimeMessage message = new MimeMessage(session);
+          message.setFrom(new InternetAddress("mojo-search-widget@redhat.com"));
+          message.setRecipients(MimeMessage.RecipientType.TO, InternetAddress.parse("mallen@redhat.com"));
+          message.setSubject("Health Check Ping Failure: code="+responseCode);
+          message.setText("Hit the url \""+url+"\" and got the response code \""+responseCode+"\" and payload \""+response.toString()+"\"");
+          Transport.send(message);
         }
+      }catch(Exception e){
+        throw new RuntimeException(e);
       }
-      
     }      
   }
 
